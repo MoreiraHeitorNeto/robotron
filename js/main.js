6b27c97ec7c5dc08d9b74a1943416f741eee8b95
User <heitor.moreira.neto@outlook.com>
2023-01-05 09:10:09 -0300

    adicionei um parametro pra minha função, que é o controle, estou pegando direto da peca que é uma classe que vai identificar o contador atual e adicionar no correto

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-const somar = document.querySelector('#somar');
-
 const controle = document.querySelectorAll('.controle-ajuste');
 
 controle.forEach((elemento) => {
     elemento.addEventListener('click', (evento) =>{
-        manipulaDados(evento.target.textContent);
-        console.log(evento.target.parentNode);
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
     })
 })
 
-function manipulaDados(operacao){
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector('.controle-contador');
     if(operacao=='+'){
-        braco.value = parseInt(braco.value)+1;
+        peca.value = parseInt(peca.value)+1;
     }else if(operacao=='-'){
-        braco.value = parseInt(braco.value)-1;
+        peca.value = parseInt(peca.value)-1;
     }
 }
